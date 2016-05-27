1e1753bc5086f453bcefac64e1838514ff2a513e
Jan Roehrich <jan@roehrich.info>
2016-05-27 21:03:04 +0200

    Scalability improvements
     * make the provisioner wait for sufficient cluster resources
     * make the slave timeout configurable
     * gracefully remove a slave from Jenkins in case of task startup
    failure

--- src/main/java/com/cloudbees/jenkins/plugins/amazonecs/ECSCloud.java
+++ src/main/java/com/cloudbees/jenkins/plugins/amazonecs/ECSCloud.java
@@ -25,21 +25,34 @@
 
 package com.cloudbees.jenkins.plugins.amazonecs;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.RegionUtils;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ecs.AmazonECSClient;
-import com.amazonaws.services.ecs.model.ClientException;
-import com.amazonaws.services.ecs.model.ContainerOverride;
-import com.amazonaws.services.ecs.model.Failure;
-import com.amazonaws.services.ecs.model.RunTaskRequest;
-import com.amazonaws.services.ecs.model.RunTaskResult;
-import com.amazonaws.services.ecs.model.StopTaskRequest;
-import com.amazonaws.services.ecs.model.TaskOverride;
+import com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsHelper;
 import com.cloudbees.jenkins.plugins.awscredentials.AmazonWebServicesCredentials;
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
-import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+
 import hudson.AbortException;
 import hudson.Extension;
 import hudson.model.Computer;
@@ -53,28 +66,17 @@ import hudson.slaves.NodeProvisioner;
 import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 import jenkins.model.JenkinsLocationConfiguration;
-import org.apache.commons.lang.StringUtils;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.DataBoundSetter;
-import org.kohsuke.stapler.QueryParameter;
-
-import javax.annotation.CheckForNull;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
 public class ECSCloud extends Cloud {
 
-    private final List<ECSTaskTemplate> templates;
+    private static final Logger LOGGER = Logger.getLogger(ECSCloud.class.getName());
+	
+    private static final int DEFAULT_SLAVE_TIMEOUT = 900;
+
+	private final List<ECSTaskTemplate> templates;
 
     /**
      * Id of the {@link AmazonWebServicesCredentials} used to connect to Amazon ECS
@@ -91,9 +93,16 @@ public class ECSCloud extends Cloud {
      */
     @CheckForNull
     private String tunnel;
+    
+    private String jenkinsUrl;
+    
+    private int slaveTimoutInSeconds;
+    
+    private ECSService ecsService;
 
-    @DataBoundConstructor
-    public ECSCloud(String name, List<ECSTaskTemplate> templates, @Nonnull String credentialsId, String cluster, String regionName) {
+	@DataBoundConstructor
+    public ECSCloud(String name, List<ECSTaskTemplate> templates, @Nonnull String credentialsId, 
+    		String cluster, String regionName, String jenkinsUrl, int slaveTimoutInSeconds) {
         super(name);
         this.credentialsId = credentialsId;
         this.cluster = cluster;
@@ -104,7 +113,25 @@ public class ECSCloud extends Cloud {
                 template.setOwer(this);
             }
         }
+        if(StringUtils.isNotBlank(jenkinsUrl)) {
+        	this.jenkinsUrl = jenkinsUrl;
+        } else {
+        	this.jenkinsUrl = JenkinsLocationConfiguration.get().getUrl();
+        }
+        
+        if(slaveTimoutInSeconds > 0) {
+        	this.slaveTimoutInSeconds = slaveTimoutInSeconds;
+        } else {
+        	this.slaveTimoutInSeconds = DEFAULT_SLAVE_TIMEOUT;
+        }
     }
+	
+	synchronized ECSService getEcsService() {
+		if(ecsService == null) {
+			ecsService = new ECSService(credentialsId, regionName);
+		}
+		return ecsService;
+	}
 
     public List<ECSTaskTemplate> getTemplates() {
         return templates;
@@ -159,60 +186,46 @@ public class ECSCloud extends Cloud {
         }
         return null;
     }
-
+    
+    AmazonECSClient getAmazonECSClient() {
+    	return getEcsService().getAmazonECSClient();
+    }
 
     @Override
     public Collection<NodeProvisioner.PlannedNode> provision(Label label, int excessWorkload) {
         try {
-
             List<NodeProvisioner.PlannedNode> r = new ArrayList<NodeProvisioner.PlannedNode>();
             final ECSTaskTemplate template = getTemplate(label);
 
             for (int i = 1; i <= excessWorkload; i++) {
-
-                r.add(new NodeProvisioner.PlannedNode(template.getDisplayName(), Computer.threadPoolForRemoting
-                  .submit(new ProvisioningCallback(template, label)), 1));
-            }
+                r.add(new NodeProvisioner.PlannedNode(template.getDisplayName(), 
+                		Computer.threadPoolForRemoting.submit(new ProvisioningCallback(template, label)), 1));
+            }           
             return r;
         } catch (Exception e) {
             LOGGER.log(Level.WARNING, "Failed to provision ECS slave", e);
             return Collections.emptyList();
         }
     }
-
-    /* package */ AmazonECSClient getAmazonECSClient() {
-        return getAmazonECSClient(credentialsId, regionName);
+    
+    void deleteTask(String taskArn, String clusterArn) {
+        getEcsService().deleteTask(taskArn, clusterArn);
     }
-
-    private static AmazonECSClient getAmazonECSClient(String credentialsId, String regionName) {
-        final AmazonECSClient client;
-        AmazonWebServicesCredentials credentials = getCredentials(credentialsId);
-        if (credentials == null) {
-            // no credentials provided, rely on com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-            // to use IAM Role define at the EC2 instance level ...
-            client = new AmazonECSClient();
-        } else {
-            if (LOGGER.isLoggable(Level.FINE)) {
-                String awsAccessKeyId = credentials.getCredentials().getAWSAccessKeyId();
-                String obfuscatedAccessKeyId = StringUtils.left(awsAccessKeyId, 4) + StringUtils.repeat("*", awsAccessKeyId.length() - (2 * 4)) + StringUtils.right(awsAccessKeyId, 4);
-                LOGGER.log(Level.FINE, "Connect to Amazon ECS with IAM Access Key {1}", new Object[]{obfuscatedAccessKeyId});
-            }
-            client = new AmazonECSClient(credentials);
-        }
-        client.setRegion(getRegion(regionName));
-        LOGGER.log(Level.FINE, "Selected Region: {0}", regionName);
-        return client;
+    
+    public String getJenkinsUrl() {
+    	return jenkinsUrl;
     }
-
-    void deleteTask(String taskArn) {
-        final AmazonECSClient client = getAmazonECSClient();
-
-        LOGGER.log(Level.INFO, "Delete ECS Slave task: {0}", taskArn);
-        try {
-            client.stopTask(new StopTaskRequest().withTask(taskArn));
-        } catch (ClientException e) {
-            LOGGER.log(Level.SEVERE, "Couldn't stop task arn " + taskArn + " caught exception: " + e.getMessage(), e);
-        }
+    
+    public void setJenkinsUrl(String jenkinsUrl) {
+    	this.jenkinsUrl = jenkinsUrl;
+    }
+    
+    public int getSlaveTimoutInSeconds() {
+    	return slaveTimoutInSeconds;
+    }
+    
+    public void setSlaveTimoutInSeconds(int slaveTimoutInSeconds) {
+    	this.slaveTimoutInSeconds = slaveTimoutInSeconds;
     }
 
     private class ProvisioningCallback implements Callable<Node> {
@@ -227,66 +240,60 @@ public class ECSCloud extends Cloud {
         }
 
         public Node call() throws Exception {
+        	final ECSSlave slave;
+        	
+        	Date now = new Date();
+        	Date timeout = new Date(now.getTime() + 1000 * slaveTimoutInSeconds);
+        	
+        	synchronized(cluster) {
+        		getEcsService().waitForSufficientClusterResources(timeout, template, cluster);        		
+        		
+	            String uniq = Long.toHexString(System.nanoTime());
+	            slave = new ECSSlave(ECSCloud.this, name + "-" + uniq, template.getRemoteFSRoot(), label == null ? null : label.toString(), new JNLPLauncher());
+	            slave.setClusterArn(cluster);	            
+	            Jenkins.getInstance().addNode(slave); 
+	            while(Jenkins.getInstance().getNode(slave.getNodeName()) == null) {
+	            	Thread.sleep(1000);
+	            }
+	            LOGGER.log(Level.INFO, "Created Slave: {0}", slave.getNodeName());	
 
-            String uniq = Long.toHexString(System.nanoTime());
-            ECSSlave slave = new ECSSlave(ECSCloud.this, name + "-" + uniq, template.getRemoteFSRoot(), label == null ? null : label.toString(), new JNLPLauncher());
-            Jenkins.getInstance().addNode(slave);
-            LOGGER.log(Level.INFO, "Created Slave: {0}", slave.getNodeName());
-
-            final AmazonECSClient client = getAmazonECSClient();
-
-            Collection<String> command = getDockerRunCommand(slave);
-            String definitionArn = template.getTaskDefinitionArn();
-            slave.setTaskDefinitonArn(definitionArn);
-
-            final RunTaskResult runTaskResult = client.runTask(new RunTaskRequest()
-              .withTaskDefinition(definitionArn)
-              .withOverrides(new TaskOverride()
-                .withContainerOverrides(new ContainerOverride()
-                  .withName("jenkins-slave")
-                  .withCommand(command)))
-              .withCluster(cluster)
-            );
-
-            if (!runTaskResult.getFailures().isEmpty()) {
-                LOGGER.log(Level.WARNING, "Slave {0} - Failure to run task with definition {1} on ECS cluster {2}", new Object[]{slave.getNodeName(), definitionArn, cluster});
-                for (Failure failure : runTaskResult.getFailures()) {
-                    LOGGER.log(Level.WARNING, "Slave {0} - Failure reason={1}, arn={2}", new Object[]{slave.getNodeName(), failure.getReason(), failure.getArn()});
-                }
-                throw new AbortException("Failed to run slave container " + slave.getNodeName());
-            }
-
-            String taskArn = runTaskResult.getTasks().get(0).getTaskArn();
-            LOGGER.log(Level.INFO, "Slave {0} - Slave Task Started : {1}", new Object[]{slave.getNodeName(), taskArn});
-            slave.setTaskArn(taskArn);
-
-            int i = 0;
-            int j = 100; // wait 100 seconds
-
-            // now wait for slave to be online
-            for (; i < j; i++) {
-                if (slave.getComputer() == null) {
-                    throw new IllegalStateException("Slave " + slave.getNodeName() + " - Node was deleted, computer is null");
-                }
-                if (slave.getComputer().isOnline()) {
-                    break;
-                }
-                LOGGER.log(Level.FINE, "Waiting for slave {0} (ecs task {1}) to connect ({2}/{3}).", new Object[]{slave.getNodeName(), taskArn, i, j});
-                Thread.sleep(1000);
-            }
-            if (!slave.getComputer().isOnline()) {
-                throw new IllegalStateException("ECS Slave " + slave.getNodeName() + " (ecs task " + taskArn + ") is not connected after " + j + " seconds");
-            }
-
-            LOGGER.log(Level.INFO, "ECS Slave " + slave.getNodeName() + " (ecs task {0}) connected", taskArn);
-            return slave;
+	            try {
+	            	String taskArn = getEcsService().runEcsTask(slave, template, cluster, getDockerRunCommand(slave));
+	            	LOGGER.log(Level.INFO, "Slave {0} - Slave Task Started : {1}", new Object[]{slave.getNodeName(), taskArn});
+	            	slave.setTaskArn(taskArn);	            
+	            } catch(AbortException ex) {
+				    Jenkins.getInstance().removeNode(slave);
+				    throw ex;
+	            }
+        	}
+        	
+        	// now wait for slave to be online
+        	while (timeout.after(new Date())) {
+        		if (slave.getComputer() == null) {
+        			throw new IllegalStateException("Slave " + slave.getNodeName() + " - Node was deleted, computer is null");
+        		}
+        		if (slave.getComputer().isOnline()) {
+        			break;
+        		}
+        		LOGGER.log(Level.FINE, "Waiting for slave {0} (ecs task {1}) to connect since {2}.", new Object[]{slave.getNodeName(), slave.getTaskArn(), now});
+        		Thread.sleep(1000);
+        	}
+        	if (!slave.getComputer().isOnline()) {
+        		final String msg = MessageFormat.format("ECS Slave {0} (ecs task {1}) not connected since {2} seconds", slave.getNodeName(), slave.getTaskArn(), now);
+        		LOGGER.log(Level.WARNING, msg);
+        		Jenkins.getInstance().removeNode(slave);
+        		throw new IllegalStateException(msg);
+        	}
+        	
+        	LOGGER.log(Level.INFO, "ECS Slave " + slave.getNodeName() + " (ecs task {0}) connected", slave.getTaskArn());
+        	return slave;
         }
     }
 
     private Collection<String> getDockerRunCommand(ECSSlave slave) {
         Collection<String> command = new ArrayList<String>();
         command.add("-url");
-        command.add(JenkinsLocationConfiguration.get().getUrl());
+        command.add(jenkinsUrl);
         if (StringUtils.isNotBlank(tunnel)) {
             command.add("-tunnel");
             command.add(tunnel);
@@ -296,24 +303,16 @@ public class ECSCloud extends Cloud {
         return command;
     }
 
-
     @Extension
     public static class DescriptorImpl extends Descriptor<Cloud> {
 
-        @Override
+    	@Override
         public String getDisplayName() {
             return Messages.DisplayName();
         }
 
         public ListBoxModel doFillCredentialsIdItems() {
-            return new StandardListBoxModel()
-              .withEmptySelection()
-              .withMatching(
-                CredentialsMatchers.always(),
-                CredentialsProvider.lookupCredentials(AmazonWebServicesCredentials.class,
-                  Jenkins.getInstance(),
-                  ACL.SYSTEM,
-                  Collections.EMPTY_LIST));
+            return AWSCredentialsHelper.doFillCredentialsIdItems(Jenkins.getActiveInstance());
         }
 
         public ListBoxModel doFillRegionNameItems() {
@@ -325,8 +324,9 @@ public class ECSCloud extends Cloud {
         }
 
         public ListBoxModel doFillClusterItems(@QueryParameter String credentialsId, @QueryParameter String regionName) {
-            try {
-                final AmazonECSClient client = getAmazonECSClient(credentialsId, regionName);
+        	ECSService ecsService = new ECSService(credentialsId, regionName);
+        	try {
+                final AmazonECSClient client = ecsService.getAmazonECSClient();
 
                 final ListBoxModel options = new ListBoxModel();
                 for (String arn : client.listClusters().getClusterArns()) {
@@ -341,8 +341,6 @@ public class ECSCloud extends Cloud {
         }
 
     }
-
-    private static final Logger LOGGER = Logger.getLogger(ECSCloud.class.getName());
 
     public static Region getRegion(String regionName) {
         if (StringUtils.isNotEmpty(regionName)) {
--- src/main/java/com/cloudbees/jenkins/plugins/amazonecs/ECSTaskTemplate.java
+++ src/main/java/com/cloudbees/jenkins/plugins/amazonecs/ECSTaskTemplate.java
@@ -25,6 +25,23 @@
 
 package com.cloudbees.jenkins.plugins.amazonecs;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+
 import com.amazonaws.services.ecs.AmazonECSClient;
 import com.amazonaws.services.ecs.model.ContainerDefinition;
 import com.amazonaws.services.ecs.model.HostEntry;
@@ -32,8 +49,12 @@ import com.amazonaws.services.ecs.model.Volume;
 import com.amazonaws.services.ecs.model.HostVolumeProperties;
 import com.amazonaws.services.ecs.model.MountPoint;
 import com.amazonaws.services.ecs.model.KeyValuePair;
+import com.amazonaws.services.ecs.model.LogConfiguration;
+import com.amazonaws.services.ecs.model.MountPoint;
 import com.amazonaws.services.ecs.model.RegisterTaskDefinitionRequest;
 import com.amazonaws.services.ecs.model.RegisterTaskDefinitionResult;
+import com.amazonaws.services.ecs.model.Volume;
+
 import hudson.Extension;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
@@ -123,6 +144,26 @@ public class ECSTaskTemplate extends AbstractDescribableImpl<ECSTaskTemplate> {
 
     private String taskDefinitionArn;
 
+    /**
+    * The log configuration specification for the container.
+    * This parameter maps to LogConfig in the Create a container section of
+    * the Docker Remote API and the --log-driver option to docker run.
+    * Valid log drivers are displayed in the LogConfiguration data type.
+    * This parameter requires version 1.18 of the Docker Remote API or greater
+    * on your container instance. To check the Docker Remote API version on
+    * your container instance, log into your container instance and run the
+    * following command: sudo docker version | grep "Server API version"
+    * The Amazon ECS container agent running on a container instance must
+    * register the logging drivers available on that instance with the
+    * ECS_AVAILABLE_LOGGING_DRIVERS environment variable before containers
+    * placed on that instance can use these log configuration options.
+    * For more information, see Amazon ECS Container Agent Configuration
+    * in the Amazon EC2 Container Service Developer Guide.
+    */
+    @CheckForNull
+    private String logDriver;
+    private List<LogDriverOption> logDriverOptions;
+
     @DataBoundConstructor
     public ECSTaskTemplate(@Nullable String label,
                            @Nonnull String image,
@@ -130,6 +171,7 @@ public class ECSTaskTemplate extends AbstractDescribableImpl<ECSTaskTemplate> {
                            int memory,
                            int cpu,
                            boolean privileged,
+                           @Nullable List<LogDriverOption> logDriverOptions,
                            @Nullable List<EnvironmentEntry> environments,
                            @Nullable List<ExtraHostEntry> extraHosts,
                            @Nullable List<MountPointEntry> mountPoints) {
@@ -139,6 +181,7 @@ public class ECSTaskTemplate extends AbstractDescribableImpl<ECSTaskTemplate> {
         this.memory = memory;
         this.cpu = cpu;
         this.privileged = privileged;
+        this.logDriverOptions = logDriverOptions;
         this.environments = environments;
         this.extraHosts = extraHosts;
         this.mountPoints = mountPoints;
@@ -152,6 +195,11 @@ public class ECSTaskTemplate extends AbstractDescribableImpl<ECSTaskTemplate> {
     @DataBoundSetter
     public void setJvmArgs(String jvmArgs) {
         this.jvmArgs = StringUtils.trimToNull(jvmArgs);
+    }
+
+    @DataBoundSetter
+    public void setLogDriver(String logDriver) {
+        this.logDriver = StringUtils.trimToNull(logDriver);
     }
 
     public String getLabel() {
@@ -184,6 +232,53 @@ public class ECSTaskTemplate extends AbstractDescribableImpl<ECSTaskTemplate> {
 
     public boolean getPrivileged() {
         return privileged;
+    }
+
+    public String getLogDriver() {
+        return logDriver;
+    }
+
+    public static class LogDriverOption extends AbstractDescribableImpl<LogDriverOption>{
+        public String name, value;
+
+        @DataBoundConstructor
+        public LogDriverOption(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "LogDriverOption{" + name + ": " + value + "}";
+        }
+
+        @Extension
+        public static class DescriptorImpl extends Descriptor<LogDriverOption> {
+            @Override
+            public String getDisplayName() {
+                return "logDriverOption";
+            }
+        }
+    }
+
+    public List<LogDriverOption> getLogDriverOptions() {
+        return logDriverOptions;
+    }
+
+    private Map<String,String> getLogDriverOptionsMap() {
+        if (null == logDriverOptions || logDriverOptions.isEmpty()) {
+            return null;
+        }
+        Map<String,String> options = new HashMap<String,String>();
+        for (LogDriverOption logDriverOption : logDriverOptions) {
+            String name = logDriverOption.name;
+            String value = logDriverOption.value;
+            if (StringUtils.isEmpty(name) || StringUtils.isEmpty(value)) {
+                continue;
+            }
+            options.put(name, value);
+        }
+        return options;
     }
 
     public String getTaskDefinitionArn() {
@@ -372,6 +467,13 @@ public class ECSTaskTemplate extends AbstractDescribableImpl<ECSTaskTemplate> {
             def.withEnvironment(new KeyValuePair()
                 .withName("JAVA_OPTS").withValue(jvmArgs))
                 .withEssential(true);
+
+        if (logDriver != null) {
+            LogConfiguration logConfig = new LogConfiguration();
+            logConfig.setLogDriver(logDriver);
+            logConfig.setOptions(getLogDriverOptionsMap());
+            def.withLogConfiguration(logConfig);
+        }
 
         return new RegisterTaskDefinitionRequest()
             .withFamily("jenkins-slave")
